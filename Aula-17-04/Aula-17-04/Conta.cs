4071fbedf349ed96bf0461a83142b9ef2f8290fa
MAYARA LUPETI TURBIANI <mayara.lturbiani@senacsp.edu.br>
2023-04-17 20:43:11 -0300

    aula 17-04

---
+++ Aula-17-04/Aula-17-04/Conta.cs
@@ -0,0 +1,24 @@
+﻿public class Conta
+{
+    int idConta;
+    int agencia;
+    int banco;
+    double saldo;
+    TipoConta tipoConta;
+
+    public Conta(int idConta, int agencia, int banco)
+    {
+        this.idConta = idConta;
+        this.agencia = agencia;
+        this.banco = banco;
+        tipoConta = TipoConta.ContaFisica;
+    }
+
+    public Conta(int idConta, int agencia, int banco, TipoConta tipoConta)
+    {
+        this.idConta = idConta;
+        this.agencia = agencia;
+        this.banco = banco;
+        this.tipoConta = tipoConta;
+    }
+}
---
+++ Aula-17-04/Aula-17-04/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿public class Pessoa
+{
+    string nome;
+    Conta conta;
+
+    public Pessoa(string nome, Conta conta)
+    {
+        this.nome = nome;
+        this.conta = conta;
+    }
+}
